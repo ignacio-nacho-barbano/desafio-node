cfb3ca67571700f2f79ffddd4ff289044c2695ae
virginia-costa <costa.virginia7@gmail.com>
2023-05-03 10:49:57 -0300

    Importation route delete

--- backend/routes/inmueblesRoutes.js
+++ backend/routes/inmueblesRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { saludo, mostrarInmuebles, mostrarInmuebleId, nuevoInmueble, registroUsuario, loginUsuario, editarInmueble } = require("../controllers/inmueblesControllers");
+const { saludo, mostrarInmuebles, mostrarInmuebleId, nuevoInmueble, registroUsuario, loginUsuario, editarInmueble, eliminarInmueble } = require("../controllers/inmueblesControllers");
 const routes = express.Router();
 
 const { runValidation } = require("../middleware/validator/index");
@@ -10,10 +10,11 @@ routes.get("/", saludo);
 routes.get("/inmuebles", verifyToken, runValidation, validarUsuarioCliente, mostrarInmuebles);
 routes.get("/inmuebles/:id", verifyToken, runValidation, validarUsuarioCliente, validarUsuarioAdmin, validarUsuarioGerente, mostrarInmuebleId);
 routes.post("/inmuebles/nuevo", verifyToken, runValidation, validarUsuarioAdmin, nuevoInmueble);
-routes.post("/registro", registroUsuario)
-routes.post("/login", loginUsuario)
+routes.post("/registro", registroUsuario);
+routes.post("/login", loginUsuario);
 
-routes.put("/inmuebles/editar/:id", editarInmueble)
+routes.put("/inmuebles/editar/:id", editarInmueble);
+routes.delete("inmuebles/elimnar/:id", eliminarInmueble);
 
 
 module.exports = routes;
